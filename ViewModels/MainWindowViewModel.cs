645af6be97896407d0650df962371eaa57be2d7a
CSUwangj <CSUwangj@protonmail.com>
2021-12-01 02:31:32 +0800

    feat: init whole project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace md2docx_avalon.ViewModels {
+    public class MainWindowViewModel : ViewModelBase {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
